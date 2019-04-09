3dac8707db2da13f76038b4ffe92f902ca00a7e2
Wolfgang Tom <mail@wolfgangtom.de>
2019-04-09 13:02:40 +0200

    phpdotenv version 3.3

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,6 @@
 @setup
 	require __DIR__.'/vendor/autoload.php';
-	$dotenv = new Dotenv\Dotenv(__DIR__);
+	$dotenv = Dotenv\Dotenv::create(__DIR__);
 	try {
 		$dotenv->load();
 		$dotenv->required(['DEPLOY_SERVER', 'DEPLOY_REPOSITORY', 'DEPLOY_PATH'])->notEmpty();
